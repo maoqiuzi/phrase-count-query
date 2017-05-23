b0974f530b34be79d7c43de14f8f775fcec21920
maoqiuzi <greywilliam333374@gmail.com>
2017-05-23 14:09:11 -0400

    first commit

---
+++ src/main/java/com/maoqiuzi/esplugins/psqplugin/PhraseCountQuery.java
@@ -0,0 +1,7 @@
+package com.maoqiuzi.esplugins.psqplugin;
+
+/**
+ * Created by maoqiuzi on 5/23/17.
+ */
+public class PhraseCountQuery {
+}
---
+++ src/main/java/com/maoqiuzi/esplugins/psqplugin/PhraseCountQueryPlugin.java
@@ -0,0 +1,9 @@
+package com.maoqiuzi.esplugins.psqplugin;
+
+import org.elasticsearch.plugins.SearchPlugin;
+
+/**
+ * Created by maoqiuzi on 5/23/17.
+ */
+public class PhraseCountQueryPlugin implements SearchPlugin {
+}
